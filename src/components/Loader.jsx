0ecb58e070cb16f5786ba168b48bcd3ff3ca8686
Shivanya <kshivanya82@gmail.com>
2024-07-11 23:33:29 +0530

    feat:loader created

---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Spinner } from '@chakra-ui/react'
+const Loader = () => {
+  return (
+    <div className='min-h-dvh flex items-center justify-center'><Spinner /></div>
+  )
+}
+
+export default Loader
