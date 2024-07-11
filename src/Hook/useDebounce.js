c9fb1a9f7ffd19af7357b16e375ec4def809383f
Shivanya <kshivanya82@gmail.com>
2024-07-11 23:39:43 +0530

    feat:delay time implemented

---
+++ src/Hook/useDebounce.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useEffect, useState } from "react";
+
+const useDebounce = (value, delay) => {
+    const [debounceValue, setDebounceValue] = useState(value);
+  
+    useEffect(() => {
+      const timerId = setTimeout(() => {
+        setDebounceValue(value);
+      }, delay);
+  
+      return () => {
+        clearTimeout(timerId);
+      };
+    }, [value]);
+  
+    return debounceValue;
+  };
+
+export default useDebounce
